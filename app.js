3563bf20c0e958585a4b6b9695e8bd913dd9acff
Ahno Chen <ahnochen1029@gmail.com>
2020-08-23 19:48:54 +0800

    search list

--- app.js
+++ app.js
@@ -3,18 +3,27 @@ const app = express()
 const port = 3000
 
 const exphds = require('express-handlebars')
-const resuaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphds({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: resuaurantList.results })
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  console.log(keyword)
+  const restaurant = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLocaleLowerCase())
+  })
+  res.render('index', { restaurants: restaurant, keyword: keyword })
 })
 
 app.get('/restaurants/:id', (req, res) => {
   console.log('req.params', req.params.id)
-  const restaurant = resuaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
   res.render('show', { restaurant: restaurant })
 })
 
